42ee95668b9dadcf9157de220072ce5d5b9f92af
damioune123 <damienmeur@gmail.com>
2018-02-03 22:50:02 +0100

    e2e testing ok

--- src/e2e/app.e2e-spec.ts
+++ src/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('test-app App', () => {
+describe('pfe-app App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('test-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect<any>(page.getParagraphText()).toEqual('PFE app');
   });
 });
