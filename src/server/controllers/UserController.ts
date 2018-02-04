7af1a6f56adf2a15112fbb6ebd981801157208dc
Arnelon1 <alexandremaniet@gmail.com>
2018-02-04 15:39:17 +0100

    finished first draft of user CRUD. Testing needed /!\

--- src/server/controllers/UserController.ts
+++ src/server/controllers/UserController.ts
@@ -6,6 +6,12 @@ import * as mongoose from 'mongoose';
 
 export default class UserController extends Controller {
 
+    /**
+     * Returns the user corresponding to the 'id' parameter of the GET request as JSON.
+     * @param {e.Request} request
+     * @param {e.Response} response
+     * @param {e.NextFunction} next
+     */
     @HttpGet('/:id')
     static getUser(request: express.Request, response: express.Response, next: express.NextFunction): void {
         // base sur le tuto : http://brianflove.com/2016/10/04/typescript-declaring-mongoose-schema-model/
@@ -38,10 +44,20 @@ export default class UserController extends Controller {
         }).catch(next);
     }
 
+    /**
+     * Takes JSON data from the POST request to insert a new user in the database.
+     * DTO's may be used in a future implementation.
+     * @param {e.Request} request
+     * @param {e.Response} response
+     * @param {e.NextFunction} next
+     */
     @HttpPost('/')
     static postUser(request: express.Request, response: express.Response, next: express.NextFunction): void {
         let User = mongoose.model('User', UserSchema);
-        let newUser = new User({username: 'mockRoger', password: 'mockPassRoger'});
+        /* Uncomment the following line to test the database insert with mock data
+        let mockUser = new User({username: 'mockRoger', password: 'mockPassRoger'});
+        */
+        let newUser = new User(request.body);
         newUser.save((err, createdUserObject) => {
             if (err) {
                 response.status(500).send(err);
@@ -50,25 +66,53 @@ export default class UserController extends Controller {
         });
     }
 
+    /**
+     * Updates the user corresponding to the 'id' parameter in the PUT requests with the data from the request's body.
+     * Only the fields given will be updated, others will stay the same.
+     * @param {e.Request} request
+     * @param {e.Response} response
+     * @param {e.NextFunction} next
+     */
     @HttpPut('/:id')
     static updateUser(request: express.Request, response: express.Response, next: express.NextFunction): void {
-        response.write('Update User');
-        response.status(200);
-        response.send();
+        User.findById(request.params.id, (err, user) => {
+            // Handle any possible database errors
+            if (err) {
+                response.status(500).send(err);
+            } else {
+                // Update each attribute with any possible attribute that may have been submitted in the body of the request
+                // If that attribute isn't in the requestuest body, default back to whatever it was before.
+                user.username = request.body.title || user.username;
+                user.password = request.body.description || user.password;
+                // Save the updated document back to the database
+                user.save((err2, user2) => {
+                    if (err) {
+                        response.status(500).send(err2);
+                    }
+                    response.status(200).send(user2);
+                });
+            }
+        });
     }
 
+    /**
+     * Deletes user based on the 'id' parameter in the DELETE request. Returns the ID of the deleted user in the response.
+     * @param {e.Request} request
+     * @param {e.Response} response
+     * @param {e.NextFunction} next
+     */
     @HttpDelete('/:id')
     static deleteUser(request: express.Request, response: express.Response, next: express.NextFunction): void {
-        response.write('Delete User');
-        response.status(200);
-        response.send();
-    }
-
-    @HttpGet('/all')
-    static listUsers(request: express.Request, response: express.Response, next: express.NextFunction): void {
-        response.write('Bonjour All');
-        response.status(200);
-        response.send();
+        // The user in this callback function represents the document that was found.
+        // It allows you to pass a reference back to the client in case they need a reference for some reason.
+        User.findByIdAndRemove(request.params.id, (err, user) => {
+            // We'll create a simple object to send back with a message and the id of the document that was removed
+            let responseMessage = {
+                message: 'User successfully deleted',
+                id: user._id
+            };
+            response.status(200).send(responseMessage);
+        });
     }
 
 }
