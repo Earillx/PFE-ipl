5d1f0c3b1c8be6969c67d12b81cceb8afc74f485
damioune123 <damienmeur@gmail.com>
2018-02-04 17:11:06 +0100

    ok

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,36 +1,39 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { CoreModule } from './@core/core.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+    // for development
+    // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-5/master/dist/assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpModule,
-    AppRoutingModule,
-
-    NgbModule.forRoot(),
-    ThemeModule.forRoot(),
-    CoreModule.forRoot(),
-  ],
-  bootstrap: [AppComponent],
-  providers: [
-    { provide: APP_BASE_HREF, useValue: '/' },
-  ],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [AuthGuard],
+    bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/client/vendor.ts
+++ src/client/vendor.ts
@@ -11,41 +11,16 @@ import 'bootstrap';
 // RxJS
 import 'rxjs';
 
-import '@nebular/auth';
-import '@nebular/theme';
-import 'ng2-smart-table';
-import '@asymmetrik/ngx-leaflet';
-import '@agm/core';
-import 'ng2-ckeditor';
-import 'angular2-toaster';
-import 'ngx-echarts';
-import 'ng2-tree';
-import '@ng-bootstrap/ng-bootstrap';
-import 'angular2-chartjs';
-import 'ngx-echarts';
-import '@swimlane/ngx-charts';
 
-//require( 'pace-js/pace-min.js');
-require( 'tinymce/tinymce.min.js');
-require(  'tinymce/themes/modern/theme.min.js');
-require( 'tinymce/plugins/link/plugin.min.js');
-require( 'tinymce/plugins/paste/plugin.min.js');
-require( 'tinymce/plugins/table/plugin.min.js');
-//require('./assets/vendors/echarts.min.js');
-//require(  './assets/vendors/echarts.world.min.js');
-//require('chart.js/dist/Chart.min.js');
-
-
+import '@ngx-translate/core';
+import '@ngx-translate/http-loader';
+import 'ng2-charts';
 
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import 'ionicons/scss/ionicons.scss';
-import 'font-awesome/scss/font-awesome.scss';
-import 'nebular-icons/scss/nebular-icons.scss';
-import 'pace-js/templates/pace-theme-flash.tmpl.css';
 
-import './app/@theme/styles/styles.scss';
+
+
 
