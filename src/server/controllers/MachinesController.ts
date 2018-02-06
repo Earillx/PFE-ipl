ca5161af9c34be40c81cafd3a2f8be02c33dba06
Arnelon1 <alexandremaniet@gmail.com>
2018-02-06 13:50:17 +0100

    basic qr generation works

--- src/server/controllers/MachinesController.ts
+++ src/server/controllers/MachinesController.ts
@@ -3,23 +3,26 @@ import Controller from './Controller';
 import {HttpGet, HttpPut, HttpPost, HttpDelete} from '../utils/annotations/Routes';
 import {IMachineModel, MachineSchema, Machine} from '../models/schemas/Machine';
 import * as mongoose from 'mongoose';
-import {QRCode} from 'qrcode';
+import {toFile} from 'qrcode';
 
 export default class MachinesController extends Controller {
 
-    static readonly URI = '/machines/';
+    static readonly URI = '/machines';
 
     /**
      *    @swagger
      *    /api/machines:
+     *      get:
      *
      */
-    @HttpGet('')
+    @HttpGet('/')
     static getMachines(request: express.Request, response: express.Response, next: express.NextFunction): void {
-        const qrCode = new QRCode();
-        qrCode.makeCode('http://naver.com');
-        console.log(qrCode);
-        response.status(200).send(qrCode);
+        console.log('qr method started');
+        toFile('images/qr/testQR', 'http://naver.com').then(() => {
+            console.log('qr printed');
+            response.status(200).send();
+        });
+        console.log('qr method done');
     }
 
 
--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,9 +1,15 @@
 import UserController from './UserController';
 import MeController from './MeController';
+import MachinesController from "./MachinesController";
+import ProblemController from "./ProblemController";
+import MachineController from "./MachineController";
 
 
 export default [
     UserController.export(),
-    MeController.export()
+    MeController.export(),
+    MachinesController.export(),
+    ProblemController.export(),
+    MachineController.export(),
 ];
 
