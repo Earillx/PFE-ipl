53ad9f14edf35e854f962a8839ec4f1e93d21b51
Arnelon1 <alexandremaniet@gmail.com>
2018-02-08 01:41:12 +0100

    uploadMachines done: needs testing and debugging !

--- src/server/controllers/MachinesController.ts
+++ src/server/controllers/MachinesController.ts
@@ -5,6 +5,7 @@ import {Machine} from '../models/schemas/Machine';
 import {MachineDTO} from '../../shared/MachineDTO';
 import Server from '../Server';
 import Utils from './Utils';
+import Any = jasmine.Any;
 
 
 export default class MachinesController extends Controller {
@@ -82,6 +83,10 @@ export default class MachinesController extends Controller {
     @HttpPost('/')
     static uploadMachines(request: express.Request, response: express.Response, next: express.NextFunction): void {
         const machinesRecieved: MachineDTO[] = request.body;
+        let insertedMachines: MachineDTO[];
+        let updateddMachines: MachineDTO[];
+        let disabledMachines: MachineDTO[];
+        let promises: Promise<Any>[] = [];
         /*
         logique traitement machine :
 
@@ -95,77 +100,108 @@ export default class MachinesController extends Controller {
         Machine.find({'is_available': true}, (err, machinesAvailableInDb) => {
             // looking through each machine given to us in the request
             machinesRecieved.forEach((machine) => {
-                // determining if the current machine is already in the database
-                Machine.find({'name': machine.name}, (err2, machineFound) => {
-                    if (err2) {
-                        return response.status(500).send(err2);
-                    } else if (machineFound === null) {
-                        // machine not in db => INSERT
-                        const newMachine = new Machine();
-                        newMachine.name = machine.name;
-                        newMachine.ip_address = machine.ip_address;
-                        newMachine.mac_address = machine.mac_address;
-                        newMachine.comment = machine.comment;
-                        newMachine.is_available = machine.is_available;
-                        newMachine.local = machine.local;
-                        // generate QR
-                        Utils.generateLabel(machine, Server.serverAddress, (urls: string[]) => {
-                            newMachine.url_etiquette = urls[1];
-                            newMachine.url_qr = urls[0];
-                            // Saves the new machine to the database
-                            newMachine.save({}, (err3, insertedMachine) => {
-                                if (err3) {
-                                    return response.status(500).send(err3);
-                                }
-                            });
-                        });
-                    } else {
-                        /*
-                        // machine in db
-                        if (machine.mac_address !== machineFound.mac_address ||
-                            machine.local !== machineFound.local ||
-                            machine.name !== machineFound.name ||
-                            machine.url_etiquette !== machineFound.url_etiquette ||
-                            machine.comment !== machineFound.comment ||
-                            machine.ip_address !== machineFound.ip_address ||
-                            machine.is_available !== machineFound.is_available ||
-                            machine.url_qr !== machineFound.url_qr) {
-                            // there are modified fields, need to UPDATE the found db object with the new data
-                            machineFound.name = machine.name || machineFound.name;
-                            machineFound.ip_address = machine.ip_address || machineFound.ip_address;
-                            machineFound.mac_address = machine.mac_address || machineFound.mac_address;
-                            machineFound.comment = machine.comment || machineFound.comment;
-                            machineFound.is_available = machine.is_available || machineFound.is_available;
-                            machineFound.local = machine.local || machineFound.local;
+                promises.push(new Promise((resolve, reject) => {
+                    // determining if the current machine is already in the database
+                    Machine.find({'name': machine.name}, (err2, result) => {
+                        const machineFound = result[0];
+                        if (err2) {
+                            return response.status(500).send(err2);
+                        } else if (machineFound === null) {
+                            // machine not in db => INSERT
+                            const newMachine = new Machine();
+                            newMachine.name = machine.name;
+                            newMachine.ip_address = machine.ip_address;
+                            newMachine.mac_address = machine.mac_address;
+                            newMachine.comment = machine.comment;
+                            newMachine.is_available = machine.is_available;
+                            newMachine.local = machine.local;
                             // generate QR
-                            Utils.generateLabel(machine, Server.serverAddress,(urls:string[])=>{
-                                machineFound.url_etiquette = machine.url_etiquette || machineFound.url_etiquette;
-                                machineFound.url_qr = machine.url_qr || machineFound.url_qr;
-                                // Saves the updated machine back to the database
-                                machineFound.save({}, (err4, updatedMachine) => {
-                                    if (err4) {
-                                        response.status(500).send(err4);
-                                    } else if (updatedMachine === null) {
-                                        response.status(404).send();
-                                    } else {
-                                        response.status(200).send(updatedMachine);
+                            Utils.generateLabel(machine, Server.serverAddress, (urls: string[]) => {
+                                newMachine.url_etiquette = urls[1];
+                                newMachine.url_qr = urls[0];
+                                // Saves the new machine to the database
+                                newMachine.save({}, (err3, insertedMachine) => {
+                                    if (err3) {
+                                        return response.status(500).send(err3);
                                     }
+                                    insertedMachine = insertedMachine.toObject();
+                                    insertedMachines.push(insertedMachine);
+                                    resolve();
                                 });
                             });
+                        } else {
+                            // machine in db
+                            if (machine.mac_address !== machineFound.mac_address ||
+                                machine.local !== machineFound.local ||
+                                machine.name !== machineFound.name ||
+                                machine.url_etiquette !== machineFound.url_etiquette ||
+                                machine.comment !== machineFound.comment ||
+                                machine.ip_address !== machineFound.ip_address ||
+                                machine.is_available !== machineFound.is_available ||
+                                machine.url_qr !== machineFound.url_qr) {
+                                // there are modified fields, need to UPDATE the found db object with the new data
+                                machineFound.name = machine.name || machineFound.name;
+                                machineFound.ip_address = machine.ip_address || machineFound.ip_address;
+                                machineFound.mac_address = machine.mac_address || machineFound.mac_address;
+                                machineFound.comment = machine.comment || machineFound.comment;
+                                machineFound.is_available = machine.is_available || machineFound.is_available;
+                                machineFound.local = machine.local || machineFound.local;
+                                // generate QR
+                                Utils.generateLabel(machine, Server.serverAddress, (urls: string[]) => {
+                                    machineFound.url_etiquette = machine.url_etiquette || machineFound.url_etiquette;
+                                    machineFound.url_qr = machine.url_qr || machineFound.url_qr;
+                                    // Saves the updated machine back to the database
+                                    machineFound.save({}, (err4, updatedMachine) => {
+                                        if (err4) {
+                                            return response.status(500).send(err4);
+                                        } else {
+                                            updatedMachine = updatedMachine.toObject();
+                                            updateddMachines.push(updatedMachine);
+                                            resolve();
+                                        }
+                                    });
+                                });
+                            }
+                            // retirer la machine du le liste de celles dispo en DB (les machines non traitées seront désactivées)
+                            const indexMachine = machinesAvailableInDb.indexOf(machinesAvailableInDb.find((machinePred) =>
+                                machinePred.name === machine.name));
+                            if (indexMachine > -1) {
+                                machinesAvailableInDb.splice(indexMachine, 1);
+                            }
                         }
-                        // retirer la machine du le liste de celles dispo en DB (les machines non traitées seront désactivées)
-                        const indexMachine = machinesAvailableInDb.indexOf(machinesAvailableInDb.find((machinePred) =>
-                            machinePred.name === machine.name));
-                        if (indexMachine > -1) {
-                            machinesAvailableInDb.splice(indexMachine, 1);
-                        }
-                        */
-                    }
-                });
+                    });
+
+                }));
             });
-            // disable all machines that weren't mentionned
-            machinesAvailableInDb.forEach((machinToDisable) => {
-                // todo
+            Promise.all(promises).then(value => {
+                // disable all machines that weren't mentionned
+                let promises2: Promise<Any>[] = [];
+                machinesAvailableInDb.forEach((machineToDisable) => {
+                    promises2.push(new Promise((resolve, reject) => {
+                        Machine.find({'name': machineToDisable.name}, (err2, result) => {
+                            if (err2) {
+                                return response.status(500).send(err2);
+                            } else {
+                                const disabledMachine = result[0];
+                                disabledMachine.is_available = false;
+                                disabledMachine.save({}, (err3, updatedDisabledMachine) => {
+                                    updatedDisabledMachine = updatedDisabledMachine.toObject();
+                                    disabledMachines.push(updatedDisabledMachine);
+                                    resolve();
+                                });
+                            }
+                        });
+                    }));
+                });
+                Promise.all(promises2).then(value2 => {
+                    let responseMessage = {
+                        message: 'Machines successfully uploaded',
+                        insertedMachines: insertedMachines,
+                        updatedMachines: updateddMachines,
+                        disabledMachines: disabledMachines
+                    };
+                    response.status(200).send(responseMessage);
+                });
             });
         });
     }
